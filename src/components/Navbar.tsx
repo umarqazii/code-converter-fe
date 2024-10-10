4fd24665bb5ee9dcebdf2782ef8fbf7f56f67f40
Umar Qazi <umarqazii983@gmail.com>
2024-10-10 10:20:56 +0500

    codemirror mode setting for c++ and java, aligned the input/output divs to the center, make the navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,20 +5,20 @@ const Navbar = () => {
   return (
     <div className='w-full flex items-center justify-center'>
       <div className='bg-transparent m-1 rounded-lg border-2 border-slate-700'>
-        <ul className='text-white flex items-center lg:gap-8 md:gap-6 p-2 sm:gap-2'>
+        <ul className='text-white flex items-center lg:gap-8 md:gap-6 sm:gap-4 gap-2 p-2 flex-wrap justify-center'>
           {/* Left icon */}
           <li className='flex items-center justify-center'>
             <i className="pi pi-compass text-xl bg-indigo-500 p-2 rounded-full"></i>
           </li>
-          <li className='text-slate-700'>|</li>
+          <li className='text-slate-700 hidden sm:block'>|</li>
           <li className='cursor-pointer'>Feature</li>
           <li className='cursor-pointer'>Pricing</li>
           <li className='cursor-pointer'>Insights</li>
           <li className='cursor-pointer'>Integrations</li>
-          <li className='text-slate-700'>|</li>
+          <li className='text-slate-700 hidden sm:block'>|</li>
           {/* Get Started button */}
-          <li className=''>
-            <button className='bg-gradient-to-r from-blue-800 to-purple-800 px-4 py-4 rounded-lg shadow-lg flex items-center hover:bg-gradient-to-r hover:from-purple-800 hover:to-blue-800 transition-all duration-400'>
+          <li className='w-full sm:w-auto flex justify-center mt-2 sm:mt-0'>
+            <button className='bg-gradient-to-r from-blue-800 to-purple-800 px-4 py-2 rounded-lg shadow-lg flex items-center hover:bg-gradient-to-r hover:from-purple-800 hover:to-blue-800 transition-all duration-400'>
               Get Started &nbsp; | <i className="pi pi-angle-right ml-2"></i>
             </button>
           </li>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import Navbar from '../components/Navbar';
+import 'primeicons/primeicons.css';
 import toast from 'react-hot-toast';
 import { Controlled as CodeMirror } from 'react-codemirror2';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
@@ -10,6 +11,7 @@ import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/python/python';
 import 'codemirror/mode/clike/clike'; // For C++ and Java
 import './CustomDropdown.css';
+import { read } from 'fs';
 
 const Home = () => {
   const [inputCode, setInputCode] = useState('');
@@ -90,12 +92,12 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <div className='container mx-auto px-4 py-10 flex flex-col md:flex-row md:flex-wrap items-center md:items-start justify-center' style={{ minHeight: '87vh' }}>
+      <div className="container mx-auto px-5 py-10 flex flex-col md:flex-row md:flex-wrap items-center justify-center " style={{ minHeight: '88vh' }}>
         
         {/* Input Section */}
         <div className='input-box w-full md:w-2/5 mb-6 md:mb-0'>
           <div className="flex justify-between items-center mb-4">
-            <h3 className='text-lg text-white font-semibold text-center md:text-left'>Paste your code here:</h3>
+            <h3 className='text-lg text-white font-semibold text-center md:text-left'>Paste / Type your code here:</h3>
             <select
               value={fromLanguage}
               onChange={(e) => setFromLanguage(e.target.value)}
@@ -111,7 +113,7 @@ const Home = () => {
             <CodeMirror
               value={inputCode}
               options={{
-                mode: fromLanguage.toLowerCase(),
+                mode: toLanguage === 'C++' || toLanguage === 'Java' ? 'clike' : toLanguage.toLowerCase(),
                 theme: 'material',
                 lineNumbers: true,
               }}
@@ -123,16 +125,17 @@ const Home = () => {
 
             {/* Paste Button */}
             <button 
-              className="paste-button bg-blue-600 text-white px-3 py-1 rounded-md shadow-md hover:bg-blue-500 transition duration-200 absolute top-5 right-5"
+              className="paste-button bg-slate-600 text-white px-3 py-1 rounded-md shadow-md hover:bg-slate-500 transition duration-200 absolute top-5 right-5"
               onClick={pasteFromClipboard}
             >
-              Paste
+              <i className="pi pi-clipboard"></i> Paste
             </button>
 
           </div>
 
+          <div className='flex justify-center'>
           <button
-            className='mt-4 w-full md:w-auto px-4 py-2 bg-gradient-to-r from-green-400 to-blue-500 text-white font-semibold rounded-lg shadow-md transition-transform transform hover:scale-105 focus:outline-none'
+            className='mt-4 w-full md:w-auto px-6 py-3 bg-gradient-to-r from-blue-800 to-purple-800 text-white font-semibold rounded-lg shadow-md transition-transform transform hover:scale-105 focus:outline-none'
             onClick={() => {
               try {
                 eval(inputCode);
@@ -154,23 +157,24 @@ const Home = () => {
                 });
               }
             }}
-          >
-            Check for Errors
+            >
+            Check for Errors &nbsp;<i className="pi pi-times-circle" ></i>
           </button>
+          </div>  
         </div>
 
         {/* Convert Button */}
-        <div className='processing-button flex justify-center w-full md:w-1/5 mb-6 md:mb-0'>
+        <div className='processing-button flex justify-center w-full md:w-1/5 mb-6 md:mb-0 '>
           <button
             className='w-full md:w-auto px-6 py-3 bg-gradient-to-r from-purple-600 to-indigo-600 text-white font-semibold rounded-lg shadow-md transition-transform transform hover:scale-105 focus:outline-none'
             onClick={handleConvert}
           >
-            Convert
+            Convert &nbsp;<i className="pi pi-arrow-right-arrow-left"></i>
           </button>
         </div>
 
         {/* Output Section */}
-        <div className='output-box w-full md:w-2/5'>
+        <div className='output-box w-full md:w-2/5 '>
           <div className="flex justify-between items-center mb-4">
             <h3 className='text-lg text-white font-semibold text-center md:text-left'>Your converted code here:</h3>
             <select
@@ -178,8 +182,8 @@ const Home = () => {
               onChange={(e) => setToLanguage(e.target.value)}
               className="custom-select"
             >
-              <option value="Python">Python</option>
-              <option value="JavaScript">JavaScript</option>
+              <option value="python">Python</option>
+              <option value="javascript">JavaScript</option>
               <option value="C++">C++</option>
               <option value="Java">Java</option>
             </select>
@@ -189,7 +193,9 @@ const Home = () => {
             <CodeMirror
               value={outputCode}
               options={{
-                mode: toLanguage.toLowerCase(),
+                // if the toLanguage is c++ or java, set the mode to 'clike'
+                readOnly: true,
+                mode: toLanguage === 'C++' || toLanguage === 'Java' ? 'clike' : toLanguage.toLowerCase(),
                 theme: 'material',
                 lineNumbers: true,
               }}
@@ -199,19 +205,21 @@ const Home = () => {
               className='border border-gray-300 rounded-md p-2'
             />
             <CopyToClipboard text={outputCode} onCopy={() => copyToClipboard(outputCode)}>
-              <button className="absolute top-5 right-5 bg-blue-600 text-white px-3 py-1 rounded-md shadow-md hover:bg-blue-500 transition duration-200">
-                Copy Code
+              <button className="absolute top-5 right-5 bg-slate-600 text-white px-3 py-1 rounded-md shadow-md hover:bg-slate-500 transition duration-200">
+              <i className="pi pi-copy"></i> Copy Code
               </button>
             </CopyToClipboard>
           </div>
 
           {/* Download Button */}
+          <div className='flex justify-center'>
           <button
-            className='mt-4 w-full md:w-auto px-4 py-2 bg-gradient-to-r from-yellow-400 to-red-500 text-white font-semibold rounded-lg shadow-md transition-transform transform hover:scale-105 focus:outline-none'
+            className='mt-4 w-full md:w-auto px-6 py-3 bg-gradient-to-r from-blue-800 to-purple-800 text-white font-semibold rounded-lg shadow-md transition-transform transform hover:scale-105 focus:outline-none'
             onClick={downloadCode}
           >
-            Download Code
+            Download Code &nbsp;<i className="pi pi-download" ></i>
           </button>
+          </div>
         </div>
       </div>
     </>
